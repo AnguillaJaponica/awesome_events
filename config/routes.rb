f55074fbea1efa733bd6d0a9b4be568af19eae89
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2019-12-13 13:58:14 +0900

    create login/logoff

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,5 @@ Rails.application.routes.draw do
   #   end
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
 end
